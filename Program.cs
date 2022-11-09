acf5af7511b7509de4832e0ce508efded8df096c
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-11-09 15:30:59 +0100

    milestone 2 senza exceptions

--- Program.cs
+++ Program.cs
@@ -1,64 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+bool programma = true;
 
-public class Evento
+while (programma)
 {
-    //Imposto le Properties
-    private string _titolo;
-    public string Titolo
+    //Chiedo i dati per il costruttore Evento
+    Console.Write("Inserisci il nome dell'evento: ");
+    string utenteTitolo = Console.ReadLine();
+    Console.Write("Inserisci la data dell'evento (gg/mm/yyyy): ");
+    DateTime utenteData = DateTime.Parse(Console.ReadLine());
+    Console.Write("Inserisci il numero di posti totali: ");
+    int utenteCapacitàMassima = Int32.Parse(Console.ReadLine());
+
+    //Creo il nuovo Evento
+    Evento nuovoEvento = new Evento(utenteTitolo, utenteData, utenteCapacitàMassima);
+
+    //Chiedo quanti posti l'utente vuole prenotare
+    Console.Write("Quanti posti desideri prenotare: ");
+    int postiDaPrenotare = Int32.Parse(Console.ReadLine());
+    nuovoEvento.PrenotaPosti(postiDaPrenotare);
+    Console.WriteLine();
+
+    //Stampo il numero di posti
+    Console.WriteLine("Numero di posti prenotati: " + nuovoEvento.PostiPrenotati);
+    Console.WriteLine("Numero di posti disponibili: " + nuovoEvento.PostiDisponibili());
+    Console.WriteLine();
+
+    //Chiedo all'utente se vuole disdire dei posti
+    Console.Write("Vuoi disdire dei posti (sì/no)? ");
+    string rispostaUtenteDisdetta = Console.ReadLine();
+    Console.WriteLine();
+
+    bool richiestaDisdetta = true;
+    while (richiestaDisdetta)
     {
-        get { return _titolo; }
-        set {
-            if (Titolo != "")
-            {
-                _titolo = value;
-            }
-        }
-    }
-    private DateTime _data;
-    public DateTime Data
-    {
-        get { return _data; }
-        set {
-            if (Data >= DateTime.Now)
-            {
-                _data = value;
-            }
-        }
-    }
-    public int CapienzaMassima { get; private set; }
-    public int PostiPrenotati { get; private set; }
+        if (rispostaUtenteDisdetta == "sì")
+        {
+            Console.Write("Indica il numero di posti da disdire: ");
+            int postiDaCancellare = Int32.Parse(Console.ReadLine());
+            nuovoEvento.DisdiciPosti(postiDaCancellare);
+            Console.WriteLine();
+            //Stampo il numero di posti
+            Console.WriteLine("Numero di posti prenotati: " + nuovoEvento.PostiPrenotati);
+            Console.WriteLine("Numero di posti disponibili: " + nuovoEvento.PostiDisponibili());
+            Console.WriteLine();
 
 
-    //Imposto il constructor con i vari controlli
-    public  Evento (string titolo, DateTime data, int capienzaMassima)
-        {
-        this._titolo = titolo;
-        this._data = data;
-        if(capienzaMassima > 0)
+            //Chiedo NUOVAMENTE all'utente se vuole disdire dei posti
+            Console.Write("Vuoi disdire dei posti (sì/no)? ");
+            string nuovaRispostaUtenteDisdetta = Console.ReadLine();
+            if(nuovaRispostaUtenteDisdetta == "no")
             {
-                this.CapienzaMassima = capienzaMassima;
+                Console.WriteLine("Ok, va bene!");
+                Console.WriteLine();
+                //Stampo il numero di posti
+                Console.WriteLine("Numero di posti prenotati: " + nuovoEvento.PostiPrenotati);
+                Console.WriteLine("Numero di posti disponibili: " + nuovoEvento.PostiDisponibili());
+                Console.WriteLine();
+                richiestaDisdetta =false;
             }
-        this.PostiPrenotati = 0;
+
         }
-    //METHODS
-    public void PrenotaPosti(int postiDaPrenotare)
-    {
-        if(PostiPrenotati + postiDaPrenotare <= CapienzaMassima)
+        else if(rispostaUtenteDisdetta == "no")
         {
-            this.PostiPrenotati += postiDaPrenotare;
+            Console.WriteLine("Ok, va bene!");
+            Console.WriteLine();
+            //Stampo il numero di posti
+            Console.WriteLine("Numero di posti prenotati: " + nuovoEvento.PostiPrenotati);
+            Console.WriteLine("Numero di posti disponibili: " + nuovoEvento.PostiDisponibili());
+            Console.WriteLine();
+            richiestaDisdetta = false; 
         }
     }
-    public void DisdiciPosti(int postiDaCancellare)
-    {
-        if(PostiPrenotati - postiDaCancellare >= 0)
-        {
-            this.PostiPrenotati -= postiDaCancellare;
-        }
-    }
-    public string MostraDataEvento()
-    {
-        return this.Titolo + " - " + this.Data.ToString("dd/MM/yyyy");
-    }
+    
 }
